0e534f94c3ba8331aad19fb02259badeb20919b6
Koen <162682034+KoenL-dev@users.noreply.github.com>
2026-03-10 16:33:45 +0100

    testscene setup

--- gamon-games/Scripts/Transition/TransitionManager.gd
+++ gamon-games/Scripts/Transition/TransitionManager.gd
@@ -1,5 +1,20 @@
 extends Node
 
+# GUIDE FOR THE STUPID AH TRANSITIONS!!!!!
+# Just use them whenever your changing from one scene to another, and they will handle the rest. 
+# You can choose between fade, slide, or wipe transitions.
+#
+# Usage:
+#   TransitionManager.change_scene("res://Scenes/your_scene.tscn", TransitionManager.TransitionType.FADE)
+#   TransitionManager.change_scene("res://Scenes/your_scene.tscn", TransitionManager.TransitionType.SLIDE)
+#   TransitionManager.change_scene("res://Scenes/your_scene.tscn", TransitionManager.TransitionType.WIPE)
+#
+# Transition Types:
+#   FADE  - Fade to black
+#   SLIDE - Slide in/out
+#   WIPE  - Wipe effect
+
+
 var transition_layer: CanvasLayer = null
 
 enum TransitionType {
@@ -8,6 +23,7 @@ enum TransitionType {
 	WIPE = 2
 }
 
+## Paths to transition scenes
 var transition_paths = {
 	TransitionType.FADE: "res://Scenes/Transition/FadeTransition.tscn",
 	TransitionType.SLIDE: "res://Scenes/Transition/SlideTransition.tscn",
@@ -18,20 +34,19 @@ var transition_paths = {
 func _ready() -> void:
 	_setup_transition(TransitionType.FADE)
 
-
 func _setup_transition(transition_type: int = TransitionType.FADE) -> void:
 	if transition_layer:
 		transition_layer.queue_free()
-	
-	var transition_scene = load(transition_paths[transition_type])
-	transition_layer = transition_scene.instantiate()
-	get_tree().root.add_child(transition_layer)
 
+	var transition_scene_resource = load(transition_paths[transition_type])
+	var new_transition_layer = transition_scene_resource.instantiate()
+
+	get_tree().root.add_child(new_transition_layer)
+	transition_layer = new_transition_layer
 
 func change_scene(scene_path: String, transition_type: int = TransitionType.FADE) -> void:
 	_setup_transition(transition_type)
 	await get_tree().process_frame
-	
 	await transition_layer.fade_in()
 	get_tree().change_scene_to_file(scene_path)
 	await transition_layer.fade_out()
--- gamon-games/Scripts/game.gd
+++ gamon-games/Scripts/game.gd
@@ -1,17 +1,7 @@
 extends Node
 
-@export var test_scene_path: String = "res://Scenes/character.tscn" 
-
 func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_T):
-		# Default: Fade transition
-		TransitionManager.change_scene(test_scene_path, TransitionManager.TransitionType.FADE)
-	if Input.is_key_pressed(KEY_Y):
-		# Slide transition
-		TransitionManager.change_scene(test_scene_path, TransitionManager.TransitionType.SLIDE)
-	if Input.is_key_pressed(KEY_U):
-		# Wipe transition
-		TransitionManager.change_scene(test_scene_path, TransitionManager.TransitionType.WIPE)
+	pass
 
 func _ready() -> void:
 	pass
