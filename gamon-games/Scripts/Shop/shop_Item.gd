cb3f785d2cc25d812a1bca54e3387ea869034d31
Koen <162682034+KoenL-dev@users.noreply.github.com>
2026-03-11 13:39:41 +0100

    Prompt
    now a prompt to press E is shown

--- gamon-games/Scripts/Shop/shop_Item.gd
+++ gamon-games/Scripts/Shop/shop_Item.gd
@@ -2,14 +2,22 @@ extends Node2D
 
 @export var item_data: ItemData
 @onready var sprite = $Sprite2D
-@onready var price_label = $Label
+@onready var price_label = $PriceLabel
+@onready var prompt = $Prompt
+
+var player_in_range: bool = false
 
 func _ready():
 	if item_data:
 		sprite.texture = item_data.texture
 		price_label.text = str(item_data.cost)
+	prompt.hide()
 
-# Assigns the item data to the item and updates the visuals accordingly
+func _unhandled_input(event):
+	if event.is_action_pressed("interact") and player_in_range:
+		buy_item()
+
+# Assigns the item data to the item and updates the visuals 
 func _on_item_data_assigned():
 	if item_data:
 		sprite.texture = item_data.texture
@@ -29,10 +37,23 @@ func buy_item():
 	else:
 		print("You're broke, adventurer.")
 
+
+## For now its just an example, but we'll need to connect it correctly to jakubs character stats 
 func apply_buff():
 	var player = get_tree().get_first_node_in_group("Player")
-	if player:
-		match item_data.buff_type:
-			"Health": player.max_health += item_data.buff_value
-			"Damage": player.attack_power += item_data.buff_value
-			"Speed": player.move_speed += item_data.buff_value
+	##if player:
+		##match item_data.buff_type:
+			##"Health": player.max_health += item_data.buff_value
+			##"Damage": player.attack_power += item_data.buff_value
+			##"Speed": player.move_speed += item_data.buff_value
+
+func _on_buy_zone_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player_in_range = true
+		prompt.show()
+
+
+func _on_buy_zone_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player_in_range = false
+		prompt.hide()
