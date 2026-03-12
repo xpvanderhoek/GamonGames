e7cd9201edb758079e62b633518fc837aa415796
Koen <162682034+KoenL-dev@users.noreply.github.com>
2026-03-12 12:18:53 +0100

    updated coins

--- gamon-games/Scripts/Shop/GoldLabel.gd
+++ gamon-games/Scripts/Shop/GoldLabel.gd
@@ -1,8 +1,8 @@
 extends Label
 
 func _ready() -> void:
-	text = str(CurrenciesManager.gold)
+	text = str(CurrenciesManager.gold) + " COINS"
 	CurrenciesManager.gold_changed.connect(_on_gold_changed)
 
 func _on_gold_changed(new_amount):
-	text = str(new_amount)
+	text = str(new_amount) + " COINS"
