1b67394c05595724364d92cc2e43817dcbe88f97
Koen <162682034+KoenL-dev@users.noreply.github.com>
2026-03-12 12:25:03 +0100

    LIRAH

--- gamon-games/Scripts/Shop/GoldLabel.gd
+++ gamon-games/Scripts/Shop/GoldLabel.gd
@@ -1,8 +1,8 @@
 extends Label
 
 func _ready() -> void:
-	text = str(CurrenciesManager.gold) + " COINS"
+	text = str(CurrenciesManager.gold) + " Lirah"
 	CurrenciesManager.gold_changed.connect(_on_gold_changed)
 
 func _on_gold_changed(new_amount):
-	text = str(new_amount) + " COINS"
+	text = str(new_amount) + " Lirah"
