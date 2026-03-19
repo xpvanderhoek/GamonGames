06723c756e67338be7946154a0fb36e63a6d833d
Assxult <farishalepoto@gmail.com>
2026-03-19 19:17:31 +0100

    Update UpgradeScreen.gd
    refactor v2 or something

--- gamon-games/Scripts/globals/UpgradeScreen.gd
+++ gamon-games/Scripts/globals/UpgradeScreen.gd
@@ -8,7 +8,8 @@ signal upgrade_selected(stat)
 @onready var button3: Button = $Panel/VBoxContainer/Label/Button3
 
 var buttons: Array = []
-var button_stat_map := {} 
+var button_to_stat_map := {} 
+var max_buttons := 3
 
 func _ready():
 	hide()
@@ -20,23 +21,23 @@ func _ready():
 func show_random_options():
 	var all_stats = PlayerStats.stats.keys()
 	all_stats.shuffle()
-	var max_buttons = all_stats.slice(0, 3) 
+	var chosen_stats = all_stats.slice(0, max_buttons)
 
-	for active_stat in range(3):  
-		var active_button = buttons[active_stat] 
-		var stat_key = max_buttons[active_stat]
+	for button_index in range(max_buttons): 
+		var current_button = buttons[button_index] 
+		var corresponding_stat = chosen_stats[button_index]
 
-		active_button.text = "%s (Lv %d)" % [stat_key.capitalize(), PlayerStats.get_upgrade_level(stat_key)]
-		button_stat_map[active_button] = stat_key  
+		current_button.text = "%s (Lv %d)" % [corresponding_stat.capitalize(), PlayerStats.get_upgrade_level(corresponding_stat)]
+		button_to_stat_map[current_button] = corresponding_stat  
 
 	show()
 
 func _on_button_pressed():
 	var pressed_button = get_pressed_button()  
-	if pressed_button and pressed_button in button_stat_map:
-		var stat = button_stat_map[pressed_button]
-		print("Selected stat:", stat)
-		emit_signal("upgrade_selected", stat)
+	if pressed_button and pressed_button in button_to_stat_map:
+		var selected_stat = button_to_stat_map[pressed_button]
+		print("Selected stat:", selected_stat)
+		emit_signal("upgrade_selected", selected_stat)
 		hide()
 
 func get_pressed_button() -> Button:
