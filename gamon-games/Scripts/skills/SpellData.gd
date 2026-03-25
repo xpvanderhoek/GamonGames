0a8eaea4a8cc733208ab9379121d57b2e13c0e51
BoTeQ <58831665+BoTeQX@users.noreply.github.com>
2026-03-25 14:56:20 +0100

    Delete gamon-games/Scripts/skills directory

--- gamon-games/Scripts/skills/SpellData.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-class_name SpellData
-
-@export var spell_id: String = ""
-@export var spell_name: String = "Attack"
-@export var icon: Texture2D
-@export var attack_power: int = 0
