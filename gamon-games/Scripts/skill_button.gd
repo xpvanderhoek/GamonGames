4f8d67f2e4bfffb3a572a29f2a1b05b2d751bf21
AdamZuur <23161353@student.hhs.nl>
2026-03-29 14:24:37 +0200

    Made all skill resources

--- gamon-games/Scripts/skill_button.gd
+++ gamon-games/Scripts/skill_button.gd
@@ -12,7 +12,6 @@ func _ready() -> void:
 		skill_branch.clear_points()
 		
 		var parent_top_middle = parent_node.global_position + Vector2(parent_node.size.x / 2, 0)
-		
 		var child_bottom_middle = self.global_position + Vector2(self.size.x / 2, self.size.y)
 		
 		var local_parent = parent_top_middle - skill_branch.global_position
@@ -29,14 +28,20 @@ func _ready() -> void:
 
 func update_line_appearance(parent_node: SkillNode) -> void:
 	if parent_node.skill.current_level == 0:
-		skill_branch.modulate = Color(0.5, 0.5, 0.5, 0.5)  # Dark and transparent
+		skill_branch.modulate = Color(0.5, 0.5, 0.5, 0.5)
 	else:
-		skill_branch.modulate = Color.WHITE  # Normal brightness
+		skill_branch.modulate = Color.WHITE
 
 func update_skill_display() -> void:
 	skill_level.text = str(skill.current_level) + "/" + str(skill.max_level)
 
 func _on_pressed() -> void:
+	if get_parent() is SkillNode:
+		var parent_node = get_parent() as SkillNode
+		if parent_node.skill.current_level == 0:
+			print("%s must be leveled first!" %parent_node.skill.skill_name)
+			return
+	
 	if skill.current_level == skill.max_level:
 		print(skill.skill_name + " is already at max level.")
 		return
@@ -48,7 +53,6 @@ func _on_pressed() -> void:
 		skill.current_level += 1
 		if skill.current_level == 1:
 			self.self_modulate = self.self_modulate * 1.42857
-			# Update line appearance for all child nodes when leveled from 0 to 1
 			_update_children_line_appearance()
 		if skill.current_level == skill.max_level:
 			pass
@@ -58,7 +62,6 @@ func _on_pressed() -> void:
 		print(str(skill.skill_name) + " leveled up to level: " + str(skill.current_level))
 
 func _update_children_line_appearance() -> void:
-	# Update line appearance for all child nodes
 	for child in get_children():
 		if child is SkillNode:
 			child.update_line_appearance(self)
