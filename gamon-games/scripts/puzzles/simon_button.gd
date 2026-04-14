1575e5d672092cd98ef2dfca50343c52a505c4f9
Assxult <farishalepoto@gmail.com>
2026-04-14 20:29:41 +0200

    base simon says logic

---
+++ gamon-games/scripts/puzzles/simon_button.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@export var idle_color: Color 
+@export var lit_color: Color 
+
+const FLASH_TIME = 0.4
+
+@onready var color_rect = $ColorRect
+
+signal button_pressed
+
+func _ready():
+	color_rect.color = idle_color
+	$Button.pressed.connect(_on_click)
+
+func flash():
+	color_rect.color = lit_color
+	await get_tree().create_timer(FLASH_TIME).timeout
+	color_rect.color = idle_color
+
+func _on_click():
+	button_pressed.emit()
---
+++ gamon-games/scripts/puzzles/simon_says.gd
@@ -0,0 +1,54 @@
+extends Node2D
+
+@onready var buttons = [$RedButton, $GreenButton, $BlueButton, $YellowButton]
+
+var sequence = []        
+var player_input = []      
+var is_showing = false    
+
+func _ready():
+	for i in buttons.size():
+		buttons[i].button_pressed.connect(_on_button_pressed.bind(i))
+	
+	await get_tree().create_timer(1.0).timeout
+	next_round()
+
+func next_round():
+	player_input.clear()
+
+	sequence.append(randi() % buttons.size())
+
+	await show_sequence()
+
+func show_sequence():
+	is_showing = true
+	
+	await get_tree().create_timer(0.5).timeout
+	
+	for idx in sequence:
+		buttons[idx].flash()
+		await get_tree().create_timer(0.8).timeout
+	
+	is_showing = false
+
+func _on_button_pressed(idx):
+	if is_showing:
+		return
+	
+	buttons[idx].flash()
+	player_input.append(idx)
+	
+	var pos = player_input.size() - 1
+	if player_input[pos] != sequence[pos]:
+		fail()
+		return
+	
+	if player_input.size() == sequence.size():
+		await get_tree().create_timer(1.0).timeout
+		next_round()
+
+func fail():
+	print("Wrong! Restarting")
+	sequence.clear()
+	await get_tree().create_timer(1.0).timeout
+	next_round()
