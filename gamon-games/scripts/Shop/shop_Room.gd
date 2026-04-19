54f9e6f2e884ffbeaf2d35cae958578e0383bc22
Koen <162682034+KoenL-dev@users.noreply.github.com>
2026-04-19 16:43:23 +0200

    Lirah is now visible

--- gamon-games/scripts/Shop/shop_Room.gd
+++ gamon-games/scripts/Shop/shop_Room.gd
@@ -14,6 +14,7 @@ const TIER_3_BASE_WEIGHT := 10.0
 @onready var exit_button = [$Parallax2D2/ExitButton/Button]
 @onready var item_tooltip_panel: Panel = $CanvasLayer/ItemTooltipPanel
 @onready var item_tooltip_label: Label = $CanvasLayer/ItemTooltipPanel/TooltipLabel
+@onready var coins: Label = $Parallax2D2/Sprite2D/CoinLabel
 
 var _parallax_offset := Vector2.ZERO
 var _shop_chatter_timer: Timer
@@ -30,6 +31,7 @@ func _ready():
 	if item_tooltip_panel:
 		item_tooltip_panel.visible = false
 
+	coins.text = str(RunData.coins)
 	spawn_shop_inventory()
 	_setup_shop_dialogue_flow()
 
@@ -57,6 +59,7 @@ func _on_shop_chatter_timeout() -> void:
 		_shop_chatter_timer.start()
 
 func _process(delta: float):
+	coins.text = str(RunData.coins)
 	var half := get_viewport_rect().size * 0.5
 	if half.x == 0.0 or half.y == 0.0:
 		return
