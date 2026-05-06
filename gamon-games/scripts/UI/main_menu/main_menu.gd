b1127780d653636a7249fd870a9690cbdf1b6409
szebkhan <shahzeb_zebkhan@outlook.com>
2026-05-06 15:36:38 +0200

    options menu created

--- gamon-games/scripts/UI/main_menu/main_menu.gd
+++ gamon-games/scripts/UI/main_menu/main_menu.gd
@@ -1,12 +1,14 @@
 extends Control
 
 const MAP_SCENE := "res://scenes/map.tscn"
+const OPTIONS_MENU := preload("res://scenes/UI/main_menu/options_menu.tscn")
 
 func _on_play_button_pressed() -> void:
 	TransitionManager.change_scene(MAP_SCENE)
 
 func _on_options_button_pressed() -> void:
-	pass
+	var options = OPTIONS_MENU.instantiate()
+	add_child(options)
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
