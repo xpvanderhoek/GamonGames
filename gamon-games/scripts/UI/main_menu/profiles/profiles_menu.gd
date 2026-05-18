55ce37b1e03bbc072091da011eca1500d38ff83f
szebkhan <shahzeb_zebkhan@outlook.com>
2026-05-18 20:57:21 +0200

    main menu changes

--- gamon-games/scripts/UI/main_menu/profiles/profiles_menu.gd
+++ gamon-games/scripts/UI/main_menu/profiles/profiles_menu.gd
@@ -4,8 +4,12 @@ extends Control
 @onready var slot_1: Button = %Slot1
 @onready var slot_2: Button = %Slot2
 @onready var slot_3: Button = %Slot3
+@onready var button: Button = $Button
 
 func _ready() -> void:
+	modulate.a = 0
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 1.0, 0.1)
 	$TitleLabel.text = title
 
 func _process(delta: float) -> void:
@@ -13,4 +17,13 @@ func _process(delta: float) -> void:
 		queue_free()
 
 func _on_profile_selected():
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 0.0, 0.1)
+	await tween.finished
+	queue_free()
+
+func _on_back_pressed() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 0.0, 0.1)
+	await tween.finished
 	queue_free()
