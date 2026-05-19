e23b3bd789294d7b07e5bbde32e39e7402ed7738
szebkhan <shahzeb_zebkhan@outlook.com>
2026-05-19 15:00:26 +0200

    fix

--- gamon-games/scripts/UI/main_menu/profiles/profiles_menu.gd
+++ gamon-games/scripts/UI/main_menu/profiles/profiles_menu.gd
@@ -4,9 +4,11 @@ extends Control
 @onready var slot_1: Button = %Slot1
 @onready var slot_2: Button = %Slot2
 @onready var slot_3: Button = %Slot3
-@onready var button: Button = $Button
+@onready var back: Button = $Back
 
 func _ready() -> void:
+	if !SaveLoad.do_any_saves_exist():
+		back.visible = false
 	modulate.a = 0
 	var tween = create_tween()
 	tween.tween_property(self, "modulate:a", 1.0, 0.1)
@@ -14,7 +16,11 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("escape"):
-		queue_free()
+		if SaveLoad.do_any_saves_exist():
+			var tween = create_tween()
+			tween.tween_property(self, "modulate:a", 0.0, 0.1)
+			await tween.finished
+			queue_free()
 
 func _on_profile_selected():
 	var tween = create_tween()
