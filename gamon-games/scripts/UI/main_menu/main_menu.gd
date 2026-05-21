3dd5eab1a9518dce09731351103a84bd75b025b3
AdamZuur <23161353@student.hhs.nl>
2026-05-21 16:28:01 +0200

    Revert "Merge branch 'main' into New-skill-tree-UI"
    This reverts commit 105963dbb5432372e69215d4411cd5336c1eddca, reversing
    changes made to 057794efd8b97b8bb5206475ed62e9898d438a95.

--- gamon-games/scripts/UI/main_menu/main_menu.gd
+++ gamon-games/scripts/UI/main_menu/main_menu.gd
@@ -3,20 +3,6 @@ extends Control
 const MAP_SCENE := "res://scenes/map/map.tscn"
 const OPTIONS_MENU := preload("res://scenes/UI/main_menu/options/options_menu.tscn")
 const SKILL_LIST_SCENE := "res://scenes/skill_list/skill_list.tscn"
-const PROFILES_MENU := preload("res://scenes/UI/main_menu/profiles/profiles_menu.tscn")
-
-@onready var profile_button: Button = $ProfileButton
-
-func _ready() -> void:
-	SaveLoad.profile_changed.connect(_on_profile_changed)
-	
-	if !SaveLoad.do_any_saves_exist():
-		var profiles = PROFILES_MENU.instantiate()
-		profiles.title = "Create a profile"
-		add_child(profiles)
-	else:
-		SaveLoad.load_on_start()
-	profile_button.text = "Profile: " + PlayerStats.profile_name
 
 func _on_play_button_pressed() -> void:
 	TransitionManager.change_scene(MAP_SCENE)
@@ -26,21 +12,7 @@ func _on_options_button_pressed() -> void:
 	add_child(options)
 
 func _on_quit_button_pressed() -> void:
-	SaveLoad.save_data()
-	SaveLoad.save_meta()
 	get_tree().quit()
 
 func _on_upgrades_button_pressed() -> void:
 	TransitionManager.change_scene(SKILL_LIST_SCENE)
-func _on_save_pressed() -> void:
-	SaveLoad.save_data()
-
-func _on_load_pressed() -> void:
-	SaveLoad.load_data(0)
-
-func _on_profile_button_pressed() -> void:
-	var profiles = PROFILES_MENU.instantiate()
-	add_sibling(profiles)
-
-func _on_profile_changed():
-	profile_button.text = "Profile: " + PlayerStats.profile_name
