0f76cc47689d37eb13251566593058207e5c6c42
xpvanderhoek <147050906+23096780@users.noreply.github.com>
2026-05-29 10:28:37 +0200

    push

---
+++ gamon-games/scripts/puzzles/chest_room/button_click.gd
@@ -0,0 +1,19 @@
+extends TextureButton
+
+const PUZZLE_SCENES := [
+	"res://scenes/puzzles/sliding_puzzle/sliding_puzzle.tscn",
+	"res://scenes/puzzles/simon_says/start_simon.tscn"
+]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	TransitionManager.change_scene(PUZZLE_SCENES[RunData.rng.randi() % PUZZLE_SCENES.size()])
